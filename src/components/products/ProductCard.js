a0065784ad77c3d2e8d922c0bea467a8fc7c7005
ShayElla101 <shayellae@gmail.com>
2022-04-11 23:40:40 +0300

    Initiated product card

--- src/components/products/ProductCard.js
+++ src/components/products/ProductCard.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-
+import { Link } from "react-router-dom";
 class ProductCard extends Component {
   state = {};
   render() {
@@ -8,7 +8,8 @@ class ProductCard extends Component {
       (price) => price.currency.symbol === "$"
     )[0];
     return (
-      <div
+      <Link
+        to={`/product/${currentProduct.id}`}
         key={currentProduct.id}
         className={
           currentProduct.inStock
@@ -16,28 +17,37 @@ class ProductCard extends Component {
             : "product-card product-out-of-stock"
         }
       >
-        <div className="product-image-container">
-          <img
-            className="product-image"
-            src={currentProduct.gallery[0]}
-            alt={currentProduct.name}
-          />
-          {currentProduct.inStock ? (
-            <div className="product-cart">
-              <i className="fa fa-shopping-cart" aria-hidden="true"></i>
-            </div>
-          ) : (
-            <div className="no-stock">
-              <p>Out of stock</p>
-            </div>
-          )}
-        </div>
+        <div
+        // key={currentProduct.id}
+        // className={
+        //   currentProduct.inStock
+        //     ? "product-card"
+        //     : "product-card product-out-of-stock"
+        // }
+        >
+          <div className="product-image-container">
+            <img
+              className="product-image"
+              src={currentProduct.gallery[0]}
+              alt={currentProduct.name}
+            />
+            {currentProduct.inStock ? (
+              <div className="product-cart">
+                <i className="fa fa-shopping-cart" aria-hidden="true"></i>
+              </div>
+            ) : (
+              <div className="no-stock">
+                <p>Out of stock</p>
+              </div>
+            )}
+          </div>
 
-        <p className="product-name">{currentProduct.name}</p>
-        <p className="product-price">
-          {productPriceCurrency.currency.symbol} {productPriceCurrency.amount}
-        </p>
-      </div>
+          <p className="product-name">{currentProduct.name}</p>
+          <p className="product-price">
+            {productPriceCurrency.currency.symbol} {productPriceCurrency.amount}
+          </p>
+        </div>
+      </Link>
     );
   }
 }
