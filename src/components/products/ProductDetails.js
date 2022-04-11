4efbfa135042a1735dbde6e1b7246b73a17feae0
ShayElla101 <shayellae@gmail.com>
2022-04-11 23:39:11 +0300

    Made product details page

---
+++ src/components/products/ProductDetails.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class ProductDetails extends Component {
+  constructor(props) {
+    super(props);
+  }
+  state = {};
+  render() {
+    return (
+      <div className="product-container">
+        <p>Product Details</p>
+      </div>
+    );
+  }
+}
+
+export default ProductDetails;
