0f711d0b29a685723bc55742ee9572eb53bf50fa
ShayElla101 <shayellae@gmail.com>
2022-04-20 21:47:53 +0300

    Fetch all currencies from the database

--- src/components/common/CurrencyOptions.js
+++ src/components/common/CurrencyOptions.js
@@ -1,14 +1,41 @@
 import React, { Component } from "react";
+import CurrencyOptionsModal from "./CurrencyModal";
 
 class CurrencyOptions extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isCurrenyModalOpen: false,
+    };
+    this.toggleCurrencyModal = this.toggleCurrencyModal.bind(this);
+  }
+
+  toggleCurrencyModal() {
+    console.log("Clicked");
+    this.setState((prevState) => ({
+      isCurrenyModalOpen: !prevState.isCurrenyModalOpen,
+    }));
+  }
+
   render() {
     return (
-      <div className="currency-container">
-        <p className="currency-icon">$</p>
-        <p>
-          <i className="fa fa-angle-down" aria-hidden="true"></i>
-        </p>
-      </div>
+      <>
+        <div className="currency-container" onClick={this.toggleCurrencyModal}>
+          <p className="currency-icon">$</p>
+          <p className="caret-icon">
+            <i
+              className={
+                this.state.isCurrenyModalOpen
+                  ? "fa fa-angle-up"
+                  : "fa fa-angle-down"
+              }
+              aria-hidden="true"
+            ></i>
+          </p>
+        </div>
+        {/* {this.state.isCurrenyModalOpen ? <CurrencyOptionsModal /> : <></>} */}
+        <CurrencyOptionsModal showOptions={this.state.isCurrenyModalOpen} />
+      </>
     );
   }
 }
