39f6777c5b880f648cf2c6db4ab3d333fa50bd37
ShayElla101 <shayellae@gmail.com>
2022-04-20 13:30:56 +0300

    Implemented product details

--- src/components/products/ProductDetails.js
+++ src/components/products/ProductDetails.js
@@ -1,17 +1,80 @@
 import React, { Component } from "react";
-
+import ProductImagePreviewer from "./ProductImagePreviewer";
+import "./productDetail.css";
+import { withRouter } from "./ProductList";
+import { client } from "../..";
+import { gql } from "@apollo/client";
+import ProductInfo from "./ProductInfo";
 class ProductDetails extends Component {
   constructor(props) {
     super(props);
+    this.state = { product: null };
   }
+
+  fetchProductDetails(id) {
+    const query = gql`
+      query ProductDetails($id: String!) {
+        product(id: $id) {
+          id
+          name
+          inStock
+          description
+          category
+          brand
+          attributes {
+            name
+            type
+            items {
+              displayValue
+              value
+            }
+          }
+          gallery
+          prices {
+            currency {
+              symbol
+            }
+            amount
+          }
+        }
+      }
+    `;
+
+    client
+      .query({
+        query: query,
+        variables: {
+          id: id,
+        },
+      })
+      .then((result) => {
+        console.log("Product ", result.data.product);
+        this.setState({
+          product: result.data.product,
+        });
+      });
+  }
+
+  componentDidMount() {
+    const { id } = this.props.match.params;
+    this.fetchProductDetails(id);
+  }
+
   state = {};
   render() {
     return (
-      <div className="product-container">
-        <p>Product Details</p>
-      </div>
+      <>
+        {this.state.product !== null ? (
+          <div className="product-container">
+            <ProductImagePreviewer gallery={this.state.product.gallery} />
+            <ProductInfo info={this.state.product}></ProductInfo>
+          </div>
+        ) : (
+          <p>Loading...</p>
+        )}
+      </>
     );
   }
 }
 
-export default ProductDetails;
+export default withRouter(ProductDetails);
