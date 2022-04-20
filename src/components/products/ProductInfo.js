2c7219f012d2d5db7271ee54867c0cd6d3d8e065
ShayElla101 <shayellae@gmail.com>
2022-04-20 14:30:00 +0300

    Check if attributes are available for a product, before displaying them.

--- src/components/products/ProductInfo.js
+++ src/components/products/ProductInfo.js
@@ -8,15 +8,31 @@ class ProductInfo extends Component {
       (price) => price.currency.symbol === "$"
     )[0];
 
+    let productHasAttributes = product.attributes.length > 0;
+
     return (
       <div>
         <p className="brand-name">{product.brand}</p>
         <p className="product-name">{product.name}</p>
-        <p className="product-label">{product.attributes[0].name}</p>
 
-        {product.attributes[0].items.map((item) => {
-          return <span className="product-price">{item.displayValue}</span>;
-        })}
+        {productHasAttributes ? (
+          <>
+            <p className="product-label">{product.attributes[0].name}</p>
+            <div className="product-attribute-container">
+              {product.attributes.length > 0 &&
+                product.attributes[0].items.map((item) => {
+                  return (
+                    <button className="product-attribute">
+                      {item.displayValue}
+                    </button>
+                  );
+                })}
+            </div>
+          </>
+        ) : (
+          <></>
+        )}
+
         <p className="product-label">Price</p>
         <p className="product-price">
           {productPrice.currency.symbol + " "}
