f3a7f5f2fa946b4d95e361fcd1bed5ec4d63a61b
ShayElla101 <shayellae@gmail.com>
2022-04-23 01:23:20 +0300

    Show Cart Navigation Bar

--- src/components/common/CartNav.js
+++ src/components/common/CartNav.js
@@ -1,11 +1,33 @@
 import React, { Component } from "react";
+import CartOverlay from "../cart/CartOverlay";
 
 class CartNav extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showCartOverlay: false,
+    };
+    this.toggleShowCartOverlay = this.toggleShowCartOverlay.bind(this);
+  }
+
+  toggleShowCartOverlay() {
+    console.log("CHANGED");
+    this.setState((prevState) => ({
+      showCartOverlay: !prevState.showCartOverlay,
+    }));
+  }
+
   render() {
     return (
-      <p className="shopping-cart-container">
-        <i className="fa fa-shopping-cart" aria-hidden="true"></i>
-      </p>
+      <>
+        <p
+          className="shopping-cart-container"
+          onClick={this.toggleShowCartOverlay}
+        >
+          <i className="fa fa-shopping-cart" aria-hidden="true"></i>
+        </p>
+        <CartOverlay showCart={this.state.showCartOverlay} />
+      </>
     );
   }
 }
