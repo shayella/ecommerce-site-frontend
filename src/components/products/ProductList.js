3e28e29e6160323df70f710439ff2f70f87a25c0
ShayElla101 <shayellae@gmail.com>
2022-04-23 01:31:05 +0300

    Product List shows the selected product category

--- src/components/products/ProductList.js
+++ src/components/products/ProductList.js
@@ -1,94 +1,18 @@
 import React, { Component } from "react";
-import { gql } from "@apollo/client/core";
-import { client } from "../..";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
 import "./product.css";
 import ProductCard from "./ProductCard";
-import { useParams } from "react-router-dom";
-
-export function withRouter(Children) {
-  return (props) => {
-    const match = { params: useParams() };
-    return <Children {...props} match={match} />;
-  };
-}
 
 class ProductList extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { data: [], categoryName: "" };
-  }
-
-  fetchCategoryProducts() {
-    let { categoryname } = this.props.match.params;
-    // this.setState({ categoryName: categoryname });
-
-    const query = gql`
-      query CategoryProducts($input: CategoryInput) {
-        category(input: $input) {
-          name
-          products {
-            id
-            name
-            brand
-            prices {
-              currency {
-                symbol
-              }
-              amount
-            }
-            inStock
-            gallery
-            description
-            category
-          }
-        }
-      }
-    `;
-
-    client
-      .query({
-        query: query,
-        variables: {
-          input: { title: categoryname },
-        },
-      })
-      .then((result) => {
-        console.log("DATA ", result.data.category.products);
-        this.setState({
-          data: result.data.category.products,
-          // categoryName: result.data.category.name,
-        });
-      });
-  }
-
-  componentDidMount() {
-    this.fetchCategoryProducts();
-  }
-
-  // componentDidUpdate(prevProps, prevState) {
-  //   if (this.props.match.params.categoryName !== this.state.categoryName) {
-  //     this.fetchCategoryProducts();
-  //   }
-  // }
-
-  // componentDidUpdate(prevProps, prevState) {
-  //   const { categoryName } = this.props.match.params;
-  //   if (categoryName !== prevState.categoryName) {
-  //     this.setState({ categoryName: categoryName });
-  //     // this.fetchCategoryProducts();
-  //   }
-  // }
-
   render() {
-    const { categoryname } = this.props.match.params;
-
+    let categoryName = this.props.selectedCategory;
+    categoryName = categoryName.charAt(0).toUpperCase() + categoryName.slice(1);
     return (
       <div>
-        <h1 className="category-name">
-          {categoryname.charAt(0).toUpperCase() + categoryname.slice(1)}
-        </h1>
+        <h1 className="category-name">{categoryName}</h1>
         <div className="products-container">
-          {this.state.data.map((product) => {
+          {this.props.products.products.map((product) => {
             return <ProductCard key={product.id} product={product} />;
           })}
         </div>
@@ -97,5 +21,15 @@ class ProductList extends Component {
   }
 }
 
-export default withRouter(ProductList);
+PropTypes.ProductList = {
+  fetchProductByCategory: PropTypes.func.isRequired,
+  products: PropTypes.object,
+};
+
+const mapStateToProps = (state) => ({
+  products: state.products,
+  selectedCategory: state.categories.selectedCategory,
+});
+
+export default connect(mapStateToProps)(ProductList);
 // export default ProductList;
