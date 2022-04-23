1b7eea076f9d00c2c7e2131e399dec4c7757085b
ShayElla101 <shayellae@gmail.com>
2022-04-23 23:35:32 +0300

    On Add Product to cart, add another product if item exists, or add another product

--- src/reducers/cartReducer.js
+++ src/reducers/cartReducer.js
@@ -10,7 +10,46 @@ const initialState = {
 const cartReducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_PRODUCT_TO_CART:
-      return { items: [...state.items, action.payload] };
+      // Check if product exists
+      if (
+        state.items.some(
+          (e) =>
+            e.id === action.payload.id &&
+            Object.keys(e.selectedAttributes).every(
+              (key) =>
+                e.selectedAttributes[key] ===
+                action.payload.selectedAttributes[key]
+            )
+        )
+      ) {
+        //  If product already exists and the selected attributes are the same, use increase the count of the product
+        let newItems = state.items.map((item) =>
+          item.id === action.payload.id
+            ? { ...item, count: item.count + 1 }
+            : item
+        );
+        return { items: [...newItems] };
+      } else {
+        return { items: [...state.items, action.payload] };
+      }
+
+    // console.log("Payload ", action.payload.id);
+    // let productInCart = state.items.filter(
+    //   (item) => (item.id = action.payload.id)
+    // )[0];
+
+    // console.log("Product in cart ", productInCart);
+
+    // if (productInCart !== undefined) {
+    //   let newItems = state.items.map((item) =>
+    //     item.id === action.payload.id
+    //       ? { ...item, count: item.count + 1 }
+    //       : item
+    //   );
+    //   return { items: [...newItems] };
+    // } else {
+    //   return { items: [...state.items, action.payload] };
+    // }
 
     case REMOVE_PRODUCT_FROM_CART:
       return {
