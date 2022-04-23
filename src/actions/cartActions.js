709df192022f275d8e68ee9523fd8d74d8f74257
ShayElla101 <shayellae@gmail.com>
2022-04-24 00:01:04 +0300

    Added actions to increase and decrease the product count in the store

--- src/actions/cartActions.js
+++ src/actions/cartActions.js
@@ -1,4 +1,9 @@
-import { ADD_PRODUCT_TO_CART, REMOVE_PRODUCT_FROM_CART } from "./types";
+import {
+  ADD_PRODUCT_TO_CART,
+  DECREASE_PRODUCT_COUNT_IN_CART,
+  INCREASE_PRODUCT_COUNT_IN_CART,
+  REMOVE_PRODUCT_FROM_CART,
+} from "./types";
 
 export const addProductToCart = (product) => (dispatch) => {
   console.log("Product Id");
@@ -12,5 +17,19 @@ export const deleteProductToCart = (productId) => (dispatch) => {
   dispatch({
     type: REMOVE_PRODUCT_FROM_CART,
     payload: productId,
+  });
+};
+
+export const increaseProductInCart = (product) => (dispatch) => {
+  dispatch({
+    type: INCREASE_PRODUCT_COUNT_IN_CART,
+    payload: product,
+  });
+};
+
+export const decreaseProductInCart = (product) => (dispatch) => {
+  dispatch({
+    type: DECREASE_PRODUCT_COUNT_IN_CART,
+    payload: product,
   });
 };
