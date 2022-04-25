530f10b123fbdaa0cc18be52aa0c702e66b09750
ShayElla101 <shayellae@gmail.com>
2022-04-25 23:35:27 +0300

    Checkout page implemented

---
+++ src/components/cart/Checkout.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "../errors/styles.css";
+
+class CheckOut extends Component {
+  render() {
+    return (
+      <div className="centered">
+        <h1>CheckOut</h1>
+        <p>Thanks for shopping with us :)</p>
+      </div>
+    );
+  }
+}
+
+export default CheckOut;
