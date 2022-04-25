f2bffeef4dba01320df243404d364fe402431cc4
ShayElla101 <shayellae@gmail.com>
2022-04-25 23:26:51 +0300

    Switched out font awesome icon form svg icon

--- src/components/common/CartNav.js
+++ src/components/common/CartNav.js
@@ -1,13 +1,19 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
 import CartOverlay from "../cart/CartOverlay";
+import Badge from "./Badge";
+import EmptyCartImage from "../../images/empty-cart";
 
 class CartNav extends Component {
   constructor(props) {
     super(props);
     this.state = {
       showCartOverlay: false,
+      qty: 0,
     };
     this.toggleShowCartOverlay = this.toggleShowCartOverlay.bind(this);
+    this.hideCartOverlayModal = this.hideCartOverlayModal.bind(this);
   }
 
   toggleShowCartOverlay() {
@@ -17,19 +23,59 @@ class CartNav extends Component {
     }));
   }
 
+  hideCartOverlayModal() {
+    this.setState((prevState) => ({
+      showCartOverlay: false,
+    }));
+  }
+
+  calculateTotal() {
+    let totalItems = 0;
+
+    this.props.cart.items.forEach((item) => {
+      totalItems = totalItems + item.count;
+    });
+
+    this.setState({ qty: totalItems });
+  }
+
+  componentDidMount() {
+    this.calculateTotal();
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.cart !== prevProps.cart) {
+      this.calculateTotal();
+    }
+  }
+
   render() {
     return (
-      <>
-        <p
+      <div>
+        <div
           className="shopping-cart-container"
           onClick={this.toggleShowCartOverlay}
         >
-          <i className="fa fa-shopping-cart" aria-hidden="true"></i>
-        </p>
-        <CartOverlay showCart={this.state.showCartOverlay} />
-      </>
+          {/* <i className="fa fa-shopping-cart" aria-hidden="true"></i> */}
+          <EmptyCartImage size={20} color="black" />
+          <Badge value={this.state.qty} />
+        </div>
+        <CartOverlay
+          showCart={this.state.showCartOverlay}
+          toggleCart={this.toggleShowCartOverlay}
+          hideCart={this.hideCartOverlayModal}
+        />
+      </div>
     );
   }
 }
 
-export default CartNav;
+CartNav.propTypes = {
+  cart: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  cart: state.cart,
+});
+
+export default connect(mapStateToProps)(CartNav);
