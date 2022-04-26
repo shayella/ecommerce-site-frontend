9e38cba00a933e3a6da6fc300aa6e68d5a1cf7e1
ShayElla101 <shayellae@gmail.com>
2022-04-26 16:09:02 +0300

    Made Store persistent

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import store from "./store";
+import store, { persistor } from "./store";
 import App from "./App";
+import { PersistGate } from "redux-persist/integration/react";
 import NavBar from "./components/common/NavBar";
 import ProductList from "./components/products/ProductList";
 import reportWebVitals from "./reportWebVitals";
@@ -23,16 +24,18 @@ ReactDOM.render(
   <React.StrictMode>
     <ApolloProvider client={client}>
       <Provider store={store}>
-        <Router>
-          <NavBar></NavBar>
-          <Routes>
-            <Route exact path="" element={<ProductList />} />
-            <Route exact path="product/:id" element={<ProductDetails />} />
-            <Route exact path="cart" element={<MyCart />} />
-            <Route exact path="checkout" element={<CheckOut />} />
-            <Route exact path="*" element={<PageNotFound />} />
-          </Routes>
-        </Router>
+        <PersistGate loading={null} persistor={persistor}>
+          <Router>
+            <NavBar></NavBar>
+            <Routes>
+              <Route exact path="" element={<ProductList />} />
+              <Route exact path="product/:id" element={<ProductDetails />} />
+              <Route exact path="cart" element={<MyCart />} />
+              <Route exact path="checkout" element={<CheckOut />} />
+              <Route exact path="*" element={<PageNotFound />} />
+            </Routes>
+          </Router>
+        </PersistGate>
       </Provider>
     </ApolloProvider>
   </React.StrictMode>,
--- src/store.js
+++ src/store.js
@@ -1,19 +1,31 @@
 import { createStore, applyMiddleware, compose } from "redux";
 // import { configureStore } from "@reduxjs/toolkit";
 import thunk from "redux-thunk";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import rootReducer from "./reducers";
 
 const initialState = {};
 const middleware = [thunk];
 
+const persistConfig = {
+  key: "root",
+  storage: storage,
+  whitelist: ["categories", "currencies", "cart"],
+};
+
+const pReducer = persistReducer(persistConfig, rootReducer);
+
 const store = createStore(
-  rootReducer,
+  pReducer,
   initialState,
   compose(
     applyMiddleware(...middleware),
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
+
+export const persistor = persistStore(store);
 
 // const store = configureStore({
 //   reducer: {
