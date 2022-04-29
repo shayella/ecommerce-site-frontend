a809b20bdfeed1716fad56885085e0d053ac505e
ShayElla101 <shayellae@gmail.com>
2022-04-29 13:57:09 +0300

    Modified routerUtil, removed eslint

--- src/components/common/routerUtil.js
+++ src/components/common/routerUtil.js
@@ -2,7 +2,6 @@ import React from "react";
 import { useParams } from "react-router-dom";
 
 export const withRouter = (Children) => {
-  // eslint-disable-next-line react/display-name
   return (props) => {
     const match = { params: useParams() };
     return <Children {...props} match={match} />;
