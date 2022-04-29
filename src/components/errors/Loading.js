d3d66b64428be0a31c062a5ecc7bcd0f478758df
ShayElla101 <shayellae@gmail.com>
2022-04-29 12:04:16 +0300

    Added Reusable component

---
+++ src/components/errors/Loading.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Loading extends Component {
+  render() {
+    return (
+      <div className="product-container">
+        <p className="empty-cart">Loading...</p>
+      </div>
+    );
+  }
+}
+
+export default Loading;
